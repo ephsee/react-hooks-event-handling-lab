b32a230c53575b2cd11acf6a70e696bd25248c0b
Francois Spies <fcspies@gmail.com>
2022-02-02 12:31:37 -0500

    commit tree 301e141eb02920bdd1ead6710669bf9c16263018

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    return (
+        <>
+            <button onFocus={()=> console.log('Good!')} onBlur={()=> console.log('Hey! Eyes on me!')}>Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe
+
+// When the focus event fires, use console.log to print out the text 'Good!'
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+function Keypad() {
+    return (
+        <form>
+            <input type="password" onChange={(e)=> console.log('Entering password...', e.target.value)}>
+            </input>
+        </form>
+    )
+}
+
+export default Keypad
